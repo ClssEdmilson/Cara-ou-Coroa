4465a8f9f7a5c258eb554011e4094786a5afdeea
ClssEdmilson <edmilsonclss@gmail.com>
2021-12-31 11:57:32 -0300

    mudança de tela

--- app/src/main/java/com/clssed/caraoucoroa/MainActivity.java
+++ app/src/main/java/com/clssed/caraoucoroa/MainActivity.java
@@ -2,13 +2,30 @@ package com.clssed.caraoucoroa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button jogar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        jogar = findViewById(R.id.B_Jogar);
+
+        jogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ResultadoActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
